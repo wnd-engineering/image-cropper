a2ee12c60c4a52f5f942d3ad1bac09f3ff4b183a
Yura Nosenko <yura@attendify.com>
2020-11-30 23:09:10 +0200

    Working server

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const gcs = require('@google-cloud/storage');
+const got = require('got');
+const through = require('through');
+const path = require('path');
+const sharp = require('sharp');
+const http = require("http");
 
-app.get('/', (req, res) => {
-    const name = process.env.NAME || 'World';
-    res.send(`Hello ${name}!`);
+app.get('/crop', (req, res) => {
+    const width = req.query.width;
+    const height = req.query.height;
+    const originalLink = req.query["original-link"];
+    const inputStream = got.stream(originalLink);
+    const transformer = sharp().resize(parseInt(width), parseInt(height));
+    inputStream.on('end', () => res.end());
+    inputStream.pipe(transformer).jpeg().pipe(res);
+    // req.pipe(through(write, end)).pipe(res)
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`helloworld: listening on port ${port}`);
+    console.log(`image-cropper: listening on port ${port}`);
 });
 
 module.exports = app;
